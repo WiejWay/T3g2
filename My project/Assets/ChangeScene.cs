18bfe6e97516d205b2474272f3eab501d44a2106
apisyt <86847964+apisyt@users.noreply.github.com>
2025-03-16 19:37:18 +0100

    koniec i start

---
+++ My project/Assets/ChangeScene.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ChangeScene : MonoBehaviour
+{
+    [SerializeField] private SceneAsset sceneAsset; // Wybór sceny w edytorze
+
+    public void LoadScene()
+    {
+        if (sceneAsset != null)
+        {
+            string sceneName = sceneAsset.name;
+            SceneManager.LoadScene(sceneName);
+        }
+        else
+        {
+            Debug.LogError("Nie ustawiono sceny w edytorze!");
+        }
+    }
+}
